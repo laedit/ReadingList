e5fd9b5006a0fd68f3eaca81f7a923121763dd8d
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2016-04-06 14:52:16 +0200

    Fix execProcess console log

--- GetBooksInfo.fsx
+++ GetBooksInfo.fsx
@@ -247,9 +247,9 @@ module Main =
         proc.StartInfo.RedirectStandardOutput <- true
         proc.StartInfo.RedirectStandardError <- true
         proc.ErrorDataReceived.Add(fun d -> 
-            if d.Data <> null then errorF d.Data)
+            if d.Data <> null then eprintfn d.Data)
         proc.OutputDataReceived.Add(fun d -> 
-            if d.Data <> null then messageF d.Data)
+            if d.Data <> null then printfn d.Data)
         proc.Start() |> ignore
         proc.BeginErrorReadLine()
         proc.BeginOutputReadLine()
