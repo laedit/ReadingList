6ad68527d7b892d31b367fb041954763ac672774
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2016-08-05 22:25:46 +0200

    trying to debug ftp upload

--- Publish.ps1
+++ Publish.ps1
@@ -3,7 +3,7 @@ $user = "zlaeditn12713ne"
 $pass = $env:ftp_password
 $rootDirectory = "httpdocs/addbook"
 
-Copy-Item "Publish" "Packaged"
+Copy-Item "C:\projects\readinglist\Publish" "C:\projects\readinglist\Packaged"
 New-Item "Packaged/bin" -type directory
 New-Item "Packaged/bin/app.config" -type file -value "AppVeyorApiKey:$env:config1\r\nSoleUser:$env:config2"
 
@@ -17,6 +17,7 @@ $files = Get-ChildItem -recurse $source
 foreach ($file in $files)
 {
     Write-Host "Uploading $file"
+    Write-Host "Destination: $destination/$file"
     $webclient.UploadFile("$destination/$file", $file.FullName)
 } 
 
