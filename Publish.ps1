5ab48295acdbae93ccad6dfbcb0b2e4e4eee1c99
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2016-08-05 22:18:31 +0200

    Trying to fix publish

--- Publish.ps1
+++ Publish.ps1
@@ -8,7 +8,8 @@ New-Item "Packaged/bin" -type directory
 New-Item "Packaged/bin/app.config" -type file -value "AppVeyorApiKey:$env:config1\r\nSoleUser:$env:config2"
 
 ## Get files
-$files = Get-ChildItem -recurse "Packaged"
+$dirSource = "C:\projects\readinglist\Packaged"
+$files = Get-ChildItem -recurse $dirSource
 
 ## Get ftp object
 $ftp_client = New-Object System.Net.WebClient
@@ -17,15 +18,19 @@ $ftp_address = "ftp://${user}:$pass@${server}:$rootdirectory"
 ## Make uploads
 foreach($file in $files)
 {
+    Write-Host $file
+
     $directory = "";
     $source = $file.DirectoryName + "\" + $file;
     if ($file.DirectoryName.Length -gt 0)
     {
-        $directory = $file.DirectoryName.Replace("Packaged","")
+        $directory = $file.DirectoryName.Replace($dirSource,"")
     }
     $directory += "/";
+    
     $ftp_command = $ftp_address + $directory + $file
     Write-Host $ftp_command
+    
     $uri = New-Object System.Uri($ftp_command)
     $ftp_client.UploadFile($uri, $source)
 }
