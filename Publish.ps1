ffde548df2295417835ca8a9aec75483f50f2cce
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2016-08-05 21:52:47 +0200

    Add publishing website

---
+++ Publish.ps1
@@ -0,0 +1,31 @@
+$server = "ftp://laedit.net/"
+$user = "zlaeditn12713ne"
+$pass = $env:ftp_password
+$rootDirectory = "httpdocs/addbook/"
+
+Copy-Item "Publish" "Packaged"
+New-Item "Packaged/bin" -type directory
+New-Item "Packaged/bin/app.config" -type file -value "AppVeyorApiKey:$env:config1\r\nSoleUser:$env:config2"
+
+## Get files
+$files = Get-ChildItem -recurse "Packaged"
+
+## Get ftp object
+$ftp_client = New-Object System.Net.WebClient
+$ftp_address = "ftp://${user}:$pass@${server}:$rootdirectory"
+
+## Make uploads
+foreach($file in $files)
+{
+    $directory = "";
+    $source = $file.DirectoryName + "\" + $file;
+    if ($File.DirectoryName.Length -gt 0)
+    {
+        $directory = $file.DirectoryName.Replace($Location,"")
+    }
+    $directory += "/";
+    $ftp_command = $ftp_address + $directory + $file
+   
+    $uri = New-Object System.Uri($ftp_command)
+    $ftp_client.UploadFile($uri, $source)
+}
