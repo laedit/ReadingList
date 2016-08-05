6f1a399ff8d795414c94898131e0c82e232a1168
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2016-08-05 23:04:38 +0200

    Adding ftp folder creation

--- Publish.ps1
+++ Publish.ps1
@@ -1,25 +1,45 @@
 $server = "ftp://laedit.net"
 $user = "zlaeditn12713ne"
 $pass = $env:ftp_password
-$rootDirectory = "/httpdocs/addbook"
+$rootDirectory = "/httpdocs/addbook/"
 
-Copy-Item "Publish" "Packaged" -Recurse
-New-Item "Packaged/bin" -type directory
-New-Item "Packaged/bin/app.config" -type file -value "AppVeyorApiKey:$env:config1\r\nSoleUser:$env:config2"
-
-$source = "C:\projects\readinglist\Packaged"
-
-$webclient = New-Object -TypeName System.Net.WebClient
-$webclient.Credentials = New-Object System.Net.NetworkCredential($user,$pass)
-
-$files = Get-ChildItem $source -Recurse
-
-foreach ($file in $files)
+function CreateFtpFolder ($folderToCreate)
 {
+	# Create the direct path to the folder you want to create
+	$ftpPath = "$Server/$folderToCreate"
+    # create the FtpWebRequest and configure it
+	$ftp = [System.Net.FtpWebRequest]::Create($ftpPath)
+	$ftp.Method = [System.Net.WebRequestMethods+Ftp]::MakeDirectory
+	$ftp.Credentials = New-Object System.Net.NetworkCredential($user, $pass)
+	$ftp.UsePassive = $true
+
+	$response = [System.Net.FtpWebResponse]$ftp.GetResponse()
+	$response.Close()
+}
+
+function UploadFtpFile ($fileToUpload)
+{
+    $webclient = New-Object -TypeName System.Net.WebClient
+    $webclient.Credentials = New-Object System.Net.NetworkCredential($user, $pass)
     Write-Host "Uploading $file"
     $destination = New-Object System.Uri($server + $rootDirectory + $file.Name)
     Write-Host "Destination: $destination"
     $webclient.UploadFile($destination, $file.FullName)
-} 
+    $webclient.Dispose()
+}
 
-$webclient.Dispose()
+Copy-Item "Publish" "Packaged" -Recurse
+New-Item "Packaged/bin/app.config" -type file -value "AppVeyorApiKey:$env:config1\r\nSoleUser:$env:config2"
+
+$source = "C:\projects\readinglist\Packaged"
+$files = Get-ChildItem $source -Recurse
+
+foreach	($file in $files)
+{
+    If ($file -is [System.IO.DirectoryInfo]) {
+        CreateFtpFolder $file
+    }
+    Else {
+        UploadFtpFile $file
+    }
+}
