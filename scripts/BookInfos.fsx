88c29ed6b1b088cc681c397424d7522749af9c02
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2019-02-18 14:48:19 +0100

    update to bypas new fnac protection

--- scripts/BookInfos.fsx
+++ scripts/BookInfos.fsx
@@ -41,7 +41,11 @@ let private getInfoFromHtml (specifications : HtmlNode) infoName =
 
 let private getInfosFromHtml (book : BookInfo) =
     printfn "\tbackup search"
-    let searchResult = HtmlDocument.Load(fnacSearchUrl + book.Isbn)
+    let searchRawResult = Http.RequestString
+      ( fnacSearchUrl + book.Isbn,
+        headers = [ Accept, "*/*";  "Accept-Language", "fr-FR"; "Accept-Encoding", "*"])
+
+    let searchResult = HtmlDocument.Parse(searchRawResult)
 
     let noResult =
         searchResult.Descendants
