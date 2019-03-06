ea16883e4864c7f0b4acf1deff585b2445d47e50
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2019-03-06 06:24:37 +0100

    Removing all book related processes

---
+++ BakeAndDeploy.ps1
@@ -0,0 +1,17 @@
+pretzel bake site
+
+if ($lastExitCode -ne 0)
+{
+    exit -1
+}
+else
+{
+    Write-Host "Starting deploy"
+    $envConf = '{{""default"": {{""connection"": ""ftp://{0}:{1}@laedit.net""}}}}' -f $env:ftp_user, $env:ftp_password
+    creep -e $envConf -d '{""source"": ""hash""}' -b site/_site -y
+
+    if ($lastExitCode -ne 0)
+    {
+        exit -1
+    }
+}
