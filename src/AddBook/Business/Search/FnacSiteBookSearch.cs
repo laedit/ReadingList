f53c88f525dfe971897f9740884988bfdd868d66
Jérémie Bertrand <jeremie.bertrand@outlook.fr>
2021-06-01 10:28:10 +0200

    fix fnac search

--- src/AddBook/Business/Search/FnacSiteBookSearch.cs
+++ src/AddBook/Business/Search/FnacSiteBookSearch.cs
@@ -21,7 +21,7 @@ namespace AddBook.Business.Search
             try
             {
                 var parser = new HtmlParser();
-                var htmlDoc = parser.ParseDocument(await httpClient.GetStringAsync($"http://recherche.fnac.com/SearchResult/ResultList.aspx?SCat=2!1&sft=1&Search={isbn}"));
+                var htmlDoc = parser.ParseDocument(await httpClient.GetStringAsync($"https://recherche.fnac.com/SearchResult/ResultList.aspx?SCat=2!1&sft=1&Search={isbn}"));
                 var noResultDivs = htmlDoc.QuerySelectorAll("div.txt_c.noResults.mrg_b_xlg");
                 if (noResultDivs.Any())
                 {
@@ -38,13 +38,14 @@ namespace AddBook.Business.Search
 
                 var characteristics = htmlDoc.QuerySelector("section[id='Characteristics']");
 
-                var author = WebUtility.HtmlDecode(characteristics?.GetElementsByTagName("div")?
-                    .Where(node => node.GetElementsByTagName("dt")?.First().TextContent == "Auteur")?
-                    .Select(node => node.GetElementsByTagName("dd")?.First().TextContent)?.First()).Trim();
+                var author = WebUtility.HtmlDecode(characteristics?.QuerySelectorAll("dl.characteristicsStrate__item")
+                    .First(node => node.FirstElementChild.TextContent == "Auteur")
+                    .Children[1].TextContent).Trim();
 
-                var editor = WebUtility.HtmlDecode(characteristics?.GetElementsByTagName("div")?
-                    .Where(node => node.GetElementsByTagName("dt")?.First().TextContent == "Editeur")?
-                    .Select(node => node.GetElementsByTagName("dd")?.First().TextContent)?.First()).Trim();
+
+                var editor = WebUtility.HtmlDecode(characteristics?.QuerySelectorAll("dl.characteristicsStrate__item")
+                    .First(node => node.FirstElementChild.TextContent == "Editeur")
+                    .Children[1].TextContent).Trim();
 
                 var coverUrl = htmlDoc.QuerySelector("img.js-ProductVisuals-imagePreview")?.Attributes["src"]?.Value;
 
@@ -67,9 +68,9 @@ namespace AddBook.Business.Search
                                         AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
                                     });
             httpClient.DefaultRequestHeaders.Add("Accept", "*/*");
-            httpClient.DefaultRequestHeaders.Add("Accept-Language", "en-US");
+            httpClient.DefaultRequestHeaders.Add("Accept-Language", "fr-FR");
             httpClient.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate");
-            httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/20100101 Firefox/65.0");
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/20100101 Firefox/89.0");
             // FIXME add dynamic referer?
             return httpClient;
         }
