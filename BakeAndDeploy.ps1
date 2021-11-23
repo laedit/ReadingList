2a3f9e256b321a452aeb16daf4e3f231d15be0bd
Jérémie Bertrand <jeremie.bertrand@outlook.fr>
2021-11-23 05:28:09 +0100

    source has been replaced by tracker

--- BakeAndDeploy.ps1
+++ BakeAndDeploy.ps1
@@ -9,7 +9,7 @@ else
 {
     Write-Host "Starting deploy"
     $envConf = '""environment"": {{""default"": {{""connection"": ""ftp://{0}:{1}@laedit.net""}}}}' -f $env:ftp_user, $env:ftp_password
-    creep -d "{""""source"""": """"hash"""", $envConf}" -b site/_site -y
+    creep -d "{""""tracker"""": """"hash"""", $envConf}" -b site/_site -y
 
     if ($lastExitCode -ne 0)
     {
