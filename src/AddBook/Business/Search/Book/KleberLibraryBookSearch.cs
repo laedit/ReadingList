68058cbe2d543c12f6554aaa21fe156027b253cb
Jérémie Bertrand <jeremie.bertrand@outlook.fr>
2024-04-07 16:37:09 +0200

    fix kleber search

--- src/AddBook/Business/Search/Book/KleberLibraryBookSearch.cs
+++ src/AddBook/Business/Search/Book/KleberLibraryBookSearch.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
 
 namespace AddBook.Business.Search.Book
 {
@@ -21,29 +24,58 @@ namespace AddBook.Business.Search.Book
         {
             try
             {
-                var response = await httpClient.GetAsync($"https://www.librairie-kleber.com/json/product/index/search_simple/{isbn}");
+                var parser = new HtmlParser();
+                IHtmlDocument htmlDoc;
+                var response = await httpClient.GetAsync($"https://www.librairie-kleber.com/detailrewrite.php?ean={isbn}");
 
-                if (response.StatusCode != HttpStatusCode.OK)
+                if (response.StatusCode == HttpStatusCode.OK)
                 {
-                    throw new Exception($"Error during kleber library search: {await response.Content.ReadAsStringAsync()}");
+                    htmlDoc = parser.ParseDocument(await response.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    // fallback if the advanced search fails
+                    response = await httpClient.GetAsync($"https://www.librairie-kleber.com/listeliv.php?base=paper&mots_recherche={isbn}");
+
+                    if (response.StatusCode != HttpStatusCode.OK)
+                    {
+                        throw new Exception($"Error during kleber library search: {await response.Content.ReadAsStringAsync()}");
+                    }
+
+                    htmlDoc = parser.ParseDocument(await response.Content.ReadAsStringAsync());
+
+                    var noResultDivs = htmlDoc.QuerySelectorAll("p.msg_no_result");
+                    if (noResultDivs.Any())
+                    {
+                        return Result<Book>.Fail("Data not found.");
+                    }
+
+                    var bookLink = htmlDoc.QuerySelector("h2.livre_titre>a");
+                    if (bookLink == null)
+                    {
+                        return Result<Book>.Fail("Book infos link not found.");
+                    }
+
+                    htmlDoc = parser.ParseDocument(await httpClient.GetStringAsync(bookLink.Attributes["href"].Value));
                 }
 
-                var searchResult = await response.Content.ReadFromJsonAsync<KleberLibrarySearchResult>();
-
-                if (searchResult.Products.Count == 0)
+                Book book = null;
+                var jsonLD = htmlDoc.QuerySelector("script[type='application/ld+json']")?.TextContent;
+                if (jsonLD != null)
                 {
-                    return Result<Book>.Fail("Data not found.");
+                    var jsonDoc = JsonDocument.Parse(jsonLD);
+                    book = new Book
+                    {
+                        Isbn = isbn,
+                        Title = jsonDoc.RootElement.GetProperty("name").GetString(),
+                        Author = jsonDoc.RootElement.GetProperty("author").GetProperty("name").GetString(),
+                        Editor = jsonDoc.RootElement.GetProperty("brand").GetProperty("name").GetString(),
+                        CoverUrl = jsonDoc.RootElement.GetProperty("image").GetString(),
+                        Summary = jsonDoc.RootElement.GetProperty("description").GetString()
+                    };
                 }
 
-                return Result<Book>.Success(new Book
-                {
-                    Isbn = isbn,
-                    Title = searchResult.Products[0].Name,
-                    Author = searchResult.Products[0].Authors,
-                    Editor = searchResult.Products[0].Publisher,
-                    CoverUrl = searchResult.Products[0].Photo_Recto ?? searchResult.Products[0].Photo_Moyen ?? searchResult.Products[0].Photo_Vign,
-                    Summary = searchResult.Products[0].Description
-                });
+                return Result<Book>.Success(book);
             }
             catch (Exception ex)
             {
